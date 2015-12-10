a2f4c59823644563360f23070de2a11beb6c6653
Oluwafemi Sule <thezurg07@gmail.com>
2015-12-10 01:17:20 +0100

    add controller for home, get more and get involved pages

---
+++ frontend/app/Http/Controllers/CodeSpark/AppController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\CodeSpark;
+
+use App\Http\Controllers\Controller;
+
+class AppController extends Controller
+{
+    /**
+     * Show the `Home` page.
+     *
+     * @return Response
+     */
+    public function getIndex()
+    {
+        return view('app.home');
+    }
+
+    /**
+     * Show the `Get More` page.
+     *
+     * @return Response
+     */
+    public function getMore()
+    {
+        return view('app.more');
+    }
+
+    /**
+     * Show the `Get Involved` page.
+     *
+     * @return Response
+     */
+    public function getInvolved()
+    {
+        return view('app.getinvolved');
+    }
+}
