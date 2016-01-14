e784aa15ec28a354d9530b81ef3ce882d74a5711
Olabode Ogunfuye <bunday.works@gmail.com>
2016-01-14 22:25:00 +0100

    (feature) centered briefs

--- frontend/resources/views/app/briefs.blade.php
+++ frontend/resources/views/app/briefs.blade.php
@@ -1,9 +1,20 @@
 <div class="container-fluid">
-	<div class="row">
-		<h2>Who are we</h2>
-		<h2>News and Update</h2>
-		<h2>Our Partners</h2>
-		<h2>Tools and Backbone</h2>
+	<div class="row"><!--A row-->
+		<div class="col-xs-12 text-center">
+			<h2>Who are we</h2>
+		</div>
+
+		<div class="col-xs-12 text-center">
+			<h2>News and Update</h2>
+		</div>
+
+		<div class="col-xs-12 text-center">
+			<h2>Our Partners</h2>
+		</div>
+
+		<div class="col-xs-12 text-center">
+			<h2>Tools and Backbone</h2>
+		</div>
 	</div>
 </div>
 
